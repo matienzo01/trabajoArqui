db655ec4f78c15787c29bb4cafc7bbcb80592de6
aeiou12 <82402636+aeiou12@users.noreply.github.com>
2021-05-24 00:32:13 -0300

    funcion para ver si era una constante string

--- secundarios.h
+++ secundarios.h
@@ -59,4 +59,5 @@ void muestramemoria(int[]);//despues borrar
 void muestralista(tlistastring);
 void eliminaCaracter(char *s, char c);
 int buscaConstante(tlistaES, tlistaEC,char[]);
+int esString(tlistaES,char[]);
 int buscaregistro(char[]);
